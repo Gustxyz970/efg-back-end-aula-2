ad4e4299cdc508ee963b385acacfd1a57102b5b9
Gustavo Henrique <gustavohenri970@gmail.com>
2025-08-12 16:08:15 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,9 @@
+n1 = int(input('digite um numero: '))
+
+print(f"o antecessor de  {n1} é {n1 - 1} e seu sucessor é {n1 + 1}  ")
+
+
+n1 = int(input('digite um numero: '))
+n2 = int(input('digite outro numero para somar ao primeiro: '))
+
+print(f"A soma de {n1} e {n2} é igual a {n1 + n2}")
